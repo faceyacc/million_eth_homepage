b1a44ad387516bac5c7b6db2acd83800e76c7aee
Ty Facey <justfacey@gmail.com>
2021-08-01 19:43:33 -0400

    Update MillionEtherPage.sol
    Added auctioning/bidding functionality.

--- contracts/MillionEtherPage.sol
+++ contracts/MillionEtherPage.sol
@@ -2,13 +2,52 @@
 pragma solidity >=0.4.11;
 
 contract MillionEtherPage {
+    struct Pixel {
+    address owner;
+    uint soldPrice;
+    uint[] color;
+    }
     
+    Pixel[1000][1000] public pixels;
     
-    bytes3[1000][1000] public pixels;
-
-
-    function colorPixel(uint256 x, uint256 y, bytes3 color) public {
-        pixels[x][y] = color;
+    mapping(address => uint) public pendingRefunds;
+    
+    event PixelChanged(
+        uint x, 
+        uint y,
+        address owner, 
+        uint soldPrice, 
+        uint[] color
+    );
         
+        
+  function colorPixel(uint x, uint y, uint[] memory color) payable public {
+    Pixel storage pixel = pixels[x][y];
+    require(msg.value > pixel.soldPrice);
+    
+    if(pixel.owner != address(0x0)) {
+        pendingRefunds[pixel.owner] += pixel.soldPrice;
+    }
+    
+    pixel.owner = msg.sender;
+    pixel.soldPrice = msg.value;
+    pixel.color = color;
+    
+    emit PixelChanged(x, y, pixel.owner, pixel.soldPrice, pixel.color);
   }
+    
+  
+  function withdrawRefunds() public {
+      address payable payee = payable(msg.sender);
+      uint payment = pendingRefunds[payee];
+      
+      require(payment != 0);
+      require(address(this).balance >= payment);
+      
+      pendingRefunds[payee] = 0;
+      require(payee.send(payment));
+  }
+    
+    
+    
 }
